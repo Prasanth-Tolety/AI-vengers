c78ce6d56455616ddb1f5f981011a38540856d4d
Srishashank2003 <dsrishashank308@gmail.com>
2023-04-08 10:51:03 +0530

    Add animations

---
+++ Hackfest Project/Intro page/app/src/main/java/com/example/onboarding/studentHome.java
@@ -0,0 +1,14 @@
+package com.example.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class studentHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_home);
+    }
+}
